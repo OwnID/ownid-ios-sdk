4928b71ec139a02603615c4ad7c4cebb8b5ab3d1
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-10-28 14:51:05 +0200

    Gigya: create files

--- GigyaDemo/GigyaDemoApp.swift
+++ GigyaDemo/GigyaDemoApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct GigyaDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginAndRegisterView()
         }
     }
 }
---
+++ GigyaDemo/LogInViewModel.swift
@@ -0,0 +1,47 @@
+import Combine
+import OwnIDCoreSDK
+import AccountView
+import Gigya
+
+final class LogInViewModel: ObservableObject {
+    // MARK: OwnID
+    let ownIDViewModel = OwnID.FirebaseSDK.loginViewModel()
+    
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    @Published var loggedInModel: AccountModel?
+    
+    private var bag = Set<AnyCancellable>()
+    
+    init() {
+        subscribe(to: ownIDViewModel.eventPublisher)
+    }
+    
+    func subscribe(to eventsPublisher: OwnID.LoginPublisher) {
+        eventsPublisher
+            .sink { [unowned self] event in
+                switch event {
+                case .success(let event):
+                    switch event {
+                    case .loggedIn:
+                        if let email = Auth.auth().currentUser?.email {
+                            let name = Auth.auth().currentUser?.displayName ?? ""
+                            let model = AccountModel(name: name, email: email)
+                            loggedInModel = model
+                        } else {
+                            errorMessage = "Cannot find logged in email"
+                        }
+                        
+                    case .loading:
+                        print("Loading state")
+                    }
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    errorMessage = error.localizedDescription
+                }
+            }
+            .store(in: &bag)
+    }
+}
