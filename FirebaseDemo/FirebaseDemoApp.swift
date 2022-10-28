c9294e25fc41f6f22e85475d1d7fb112a4ed8af0
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-10-28 05:36:44 +0200

    Add configure

--- FirebaseDemo/FirebaseDemoApp.swift
+++ FirebaseDemo/FirebaseDemoApp.swift
@@ -1,10 +1,23 @@
 import SwiftUI
+import FirebaseCore
+import OwnIDFirebaseSDK
 
 @main
 struct FirebaseDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        OwnID.FirebaseSDK.configure()
+        return true
     }
 }
---
+++ FirebaseDemo/RegisterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewModel.swift
+//  FirebaseDemo
+//
+//  Created by W on 10/28/22.
+//
+
+import Foundation
