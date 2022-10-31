d2500a36ace8b187d14636e6335a89e13e329835
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-10-31 19:59:13 +0100

    Add custom integration

---
+++ CustomIntegrationDemo/CustomIntegrationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomIntegrationDemoApp.swift
+//  CustomIntegrationDemo
+//
+//  Created by W on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomIntegrationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
