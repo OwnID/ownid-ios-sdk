8cd392e38b0ba0dda00319d4d0dcb0cf94a1873d
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-11-01 14:50:05 +0100

    Move network to external files

--- CustomIntegrationDemo/LogInViewModel.swift
+++ CustomIntegrationDemo/LogInViewModel.swift
@@ -2,14 +2,10 @@ import Combine
 import OwnIDCoreSDK
 import AccountView
 
-struct LoginResponse: Decodable {
-    let token: String
-}
-
 final class CustomLoginPerformer: LoginPerformer {
     func login(payload: OwnID.CoreSDK.Payload,
                email: String) -> AnyPublisher<OperationResult, OwnID.CoreSDK.Error> {
-        LogInViewModel.login(ownIdData: payload.dataContainer, email: email)
+        LoginRequest.login(ownIdData: payload.dataContainer, email: email)
     }
 }
 
@@ -59,44 +55,4 @@ final class LogInViewModel: ObservableObject {
             }
             .store(in: &bag)
     }
-    
-    static func login(ownIdData: Any?,
-                      password: String? = .none,
-                      email: String) -> AnyPublisher<OperationResult, OwnID.CoreSDK.Error> {
-        if let ownIdData = ownIdData as? [String: String], let token = ownIdData["token"] {
-            return Just(token)
-                .setFailureType(to: OwnID.CoreSDK.Error.self)
-                .eraseToAnyPublisher()
-        }
-        let payloadDict = ["email": email, "password": password]
-        return Just(payloadDict)
-            .setFailureType(to: OwnID.CoreSDK.Error.self)
-            .eraseToAnyPublisher()
-            .tryMap { try JSONSerialization.data(withJSONObject: $0) }
-            .map { payloadData -> URLRequest in
-                var request = URLRequest(url: URL(string: "https://node-mongo.custom.demo.dev.ownid.com/api/auth/login")!)
-                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.httpMethod = "POST"
-                request.httpBody = payloadData
-                return request
-            }
-            .flatMap {
-                URLSession.shared.dataTaskPublisher(for: $0)
-                    .mapError { OwnID.CoreSDK.Error.statusRequestNetworkFailed(underlying: $0) }
-                    .eraseToAnyPublisher()
-            }
-            .eraseToAnyPublisher()
-            .map { $0.data }
-            .decode(type: LoginResponse.self, decoder: JSONDecoder())
-            .map { $0.token }
-            .receive(on: DispatchQueue.main)
-            .mapError { OwnID.CoreSDK.Error.plugin(error: CustomIntegrationDemoError.loginRequestFailed(underlying: $0)) }
-            .eraseToAnyPublisher()
-    }
-}
-
-extension String: OperationResult { }
-
-enum CustomIntegrationDemoError: PluginError {
-    case loginRequestFailed(underlying: Error)
 }
--- CustomIntegrationDemo/RegisterViewModel.swift
+++ CustomIntegrationDemo/RegisterViewModel.swift
@@ -14,7 +14,7 @@ final class CustomRegistrationParameters: RegisterParameters {
 final class CustomRegistration: RegistrationPerformer {
     func register(configuration: OwnID.FlowsSDK.RegistrationConfiguration, parameters: RegisterParameters) -> AnyPublisher<OperationResult, OwnID.CoreSDK.Error> {
         let ownIdData = configuration.payload.dataContainer
-        return RegisterViewModel.register(ownIdData: ownIdData as? String,
+        return RegisterRequest.register(ownIdData: ownIdData as? String,
                                           password: OwnID.FlowsSDK.Password.generatePassword().passwordString,
                                           email: configuration.email.rawValue,
                                           name: (parameters as? CustomRegistrationParameters)?.firstName ?? "no name in CustomRegistration class")
@@ -99,55 +99,5 @@ private extension RegisterViewModel {
                 self.ownIDViewModel.shouldShowTooltip = self.ownIDViewModel.shouldShowTooltipEmailProcessingClosure(value)
             })
             .store(in: &bag)
-    }
-}
-
-extension RegisterViewModel {
-    static func register(ownIdData: String?,
-                         password: String,
-                         email: String,
-                         name: String) -> AnyPublisher<OperationResult, OwnID.CoreSDK.Error> {
-        var payloadDict = ["email": email, "password": password, "name": name]
-        if let ownIdData {
-            payloadDict["ownIdData"] = ownIdData
-        }
-        return urlSessionRequest(for: payloadDict)
-            .tryMap { response -> Void in
-                if !response.data.isEmpty {
-                    throw OwnID.CoreSDK.Error.payloadMissing(underlying: String(data: response.data, encoding: .utf8))
-                }
-            }
-            .eraseToAnyPublisher()
-            .flatMap { _ -> AnyPublisher<OperationResult, Error> in
-                LogInViewModel.login(ownIdData: ownIdData, password: password, email: email).mapError { $0 as Error }.eraseToAnyPublisher()
-            }
-            .eraseToAnyPublisher()
-            .receive(on: DispatchQueue.main)
-            .mapError { error in
-                return OwnID.CoreSDK.Error.flowCancelled
-            }
-            .eraseToAnyPublisher()
-    }
-    
-    private static func urlSessionRequest(for payloadDict: [String: Any]) -> AnyPublisher<URLSession.DataTaskPublisher.Output, OwnID.CoreSDK.Error> {
-        return Just(payloadDict)
-            .setFailureType(to: OwnID.CoreSDK.Error.self)
-            .eraseToAnyPublisher()
-            .tryMap { try JSONSerialization.data(withJSONObject: $0) }
-            .mapError { OwnID.CoreSDK.Error.initRequestBodyEncodeFailed(underlying: $0) }
-            .map { payloadData -> URLRequest in
-                var request = URLRequest(url: URL(string: "https://node-mongo.custom.demo.dev.ownid.com/api/auth/register")!)
-                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.httpMethod = "POST"
-                request.httpBody = payloadData
-                return request
-            }
-            .eraseToAnyPublisher()
-            .flatMap { request -> AnyPublisher<URLSession.DataTaskPublisher.Output, OwnID.CoreSDK.Error> in
-                URLSession.shared.dataTaskPublisher(for: request)
-                    .mapError { OwnID.CoreSDK.Error.statusRequestNetworkFailed(underlying: $0) }
-                    .eraseToAnyPublisher()
-            }
-            .eraseToAnyPublisher()
     }
 }
