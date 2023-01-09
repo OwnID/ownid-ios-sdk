7d9a8804760a9752cdcb63345ea2408c274844f1
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-01-09 09:04:04 +0100

    Add custom view target

---
+++ CustomIntegrationCustomViewDemo/com_ownid_demo_CustomIntegrationCustomViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_ownid_demo_CustomIntegrationCustomViewDemoApp.swift
+//  com.ownid.demo.CustomIntegrationCustomViewDemo
+//
+//  Created by W on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct com_ownid_demo_CustomIntegrationCustomViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
